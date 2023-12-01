c9295f74779ff35b5f40bb9a540b7ec747f4382d
Ashibaghel44 <138962968+Ashibaghel44@users.noreply.github.com>
2023-12-01 17:12:17 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule,FormGroup,FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from "@angular/forms";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Simple-Registration-Form';
+  table:boolean =false;
+  persons:any=[]
+  simpleForm!: FormGroup;
+
+  constructor(){
+  }
+  
+  ngOnInit(){
+    this.simpleForm = new FormGroup({
+      name : new FormControl('',[
+        Validators.required,
+        Validators.pattern('^[a-z A-Z]+$'),
+        Validators.minLength(3)
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(10),
+      ]),
+    })
+  }
+  
+ get name (){
+    return this.simpleForm.get('name')
+  }
+  get email (){
+    return this.simpleForm.get('email')
+  }
+  get password (){
+    return this.simpleForm.get('password')
+  }
+  onSubmit() {
+    this.persons.push(this.simpleForm.value)
+    this.table =true;
+    console.log(this.simpleForm.value)
+    this.simpleForm.reset()
+}
 }
